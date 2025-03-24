b01eb018877b4ed074e1914f2d53fb5f3c1359af
aitormolero <aitor.molero@opendeusto.es>
2025-03-24 14:01:16 +0100

    Cinema Seat Booking project structure

---
+++ CinemaSeatBooking/src/main/java/com/cinema_seat_booking/CinemaSeatBooking/CinemaSeatBookingApplication.java
@@ -0,0 +1,13 @@
+package com.cinema_seat_booking.CinemaSeatBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaSeatBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaSeatBookingApplication.class, args);
+	}
+
+}
