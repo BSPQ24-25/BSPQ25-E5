64b0c7a1734282695935d3ab015ac95b6a10b2dc
InigoDiazMunio <i.diazmunio@opendeusto.es>
2025-05-14 18:02:59 +0200

    Controllers

--- CinemaSeatBooking/src/main/java/com/cinema_seat_booking/controller/AdminController.java
+++ CinemaSeatBooking/src/main/java/com/cinema_seat_booking/controller/AdminController.java
@@ -1,10 +1,11 @@
 package com.cinema_seat_booking.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/api/admin")
 public class AdminController {
 
